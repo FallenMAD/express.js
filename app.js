60300ef24760fddb9c37b51b077874dd3abbf1f2
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-02 15:34:16 +0200

    feat: first meeting with express and its middleware

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const http = require('http');
+import http from 'http';
 
-const server = http.createServer();
+import express from 'express';
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('In the middelware');
+  next(); // allows the request to continue to the next middleware in line
+})
+
+app.use((req, res, next) => {
+  console.log('In the next middelware');
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
