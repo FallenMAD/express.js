52f6b1a7adb81072b4b8b760b1169ef720caf2a4
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-02 16:20:43 +0200

    feat: delete redundant code

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import http from 'http';
-
 import express from 'express';
 
 const app = express();
@@ -11,8 +9,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('In the next middelware');
+  res.send('<h1>Hello from Expres.js</h1>')
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
