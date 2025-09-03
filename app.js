de061f701415a8e1704fdcf1082dbec5aac576a8
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-03 11:48:15 +0200

    feat: add /add-products route and default route

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ import express from 'express';
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middelware');
-  next(); // allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+  console.log('This is always running');
+  next();
 })
 
-app.use((req, res, next) => {
-  console.log('In the next middelware');
-  res.send('<h1>Hello from Expres.js</h1>')
+app.use('/add-products', (req, res, next) => {
+  console.log('In the add-products middelware');
+  res.send('<h1>Products Page</h1>');
 })
+
+app.use('/', (req, res, next) => {
+  console.log('In the next middelware');
+  res.send('<h1>Hello from Expres.js</h1>');
+})
+
 
 app.listen(3000);
