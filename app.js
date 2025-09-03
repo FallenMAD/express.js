628563c034ea3517864b1791ba17d79788f92cde
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-03 13:20:12 +0200

    feat: add body-parser to handle POST request

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This is always running');
-  next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-products', (req, res, next) => {
-  console.log('In the add-products middelware');
-  res.send('<h1>Products Page</h1>');
+  res.send(`
+    <h1>Add Products Page</h1>
+    <form action="/product" method="POST"> 
+      <input type="text" name="title" /> 
+      <button type="submit">Send</button>
+    </form>`
+  );
+})
+
+app.use('/product', (req, res, next) => {
+  console.log(req)
+  console.log(req.body);
+  res.redirect('/')
 })
 
 app.use('/', (req, res, next) => {
-  console.log('In the next middelware');
   res.send('<h1>Hello from Expres.js</h1>');
 })
 
