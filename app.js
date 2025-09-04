21835d89547095302551492d4f5acf8b76406113
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-04 15:22:59 +0200

    feat: add adminRoutes to the instance

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 import express from 'express';
 
+import adminRoutes from './routes/admin.js';
+
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(`
-    <h1>Add Products Page</h1>
-    <form action="/product" method="POST"> 
-      <input type="text" name="title" /> 
-      <button type="submit">Send</button>
-    </form>`
-  );
-})
-
-app.post('/product', (req, res, next) => {
-  console.log(req);
-  console.log(req.body);
-  res.redirect('/');
-})
+app.use(adminRoutes);
 
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Expres.js</h1>');
