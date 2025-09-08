5cd0861aeb5dc08dd4ede3a4a7ddf1f0e15eab4c
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-08 07:56:46 +0200

    feat: add not found page

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+import path from 'path';
 import express from 'express';
 
+import { __dirname } from './utils/dirnameHelper.js';
+ 
 import adminRoutes from './routes/admin.js';
 import shopRoutes from './routes/shop.js';
 
@@ -11,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, '..', 'views', 'not-found-page.html'));
 });
 
 app.listen(3003);
