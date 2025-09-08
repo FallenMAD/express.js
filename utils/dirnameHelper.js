b8c12752099ce8ed10bcd62dd24eb12d984f6d7d
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-08 08:26:28 +0200

    feat: update path to the root directory

--- utils/dirnameHelper.js
+++ utils/dirnameHelper.js
@@ -1,6 +1,4 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-export const __filename = fileURLToPath(import.meta.url);
-
-export const __dirname = dirname(__filename);
+export const rootDir = dirname(process.argv[1]);
