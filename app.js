bb520fa5b77082a7e87d7199e734453ccfb5dbcf
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-17 11:40:56 +0200

    refactor(app.js): update a name of the serving file

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', 'not-found-page.html'));
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3003);
