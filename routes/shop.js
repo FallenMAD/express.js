ebd1e93aca037243cd6540dfe028727c60d73a1b
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-17 14:54:29 +0200

    refactor(shop.js): add new properties to render content dynamically

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,7 @@ import { products as adminData } from './admin.js';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop.pug');
+  res.render('shop.pug', { products: adminData, docTitle: 'Shop' });
 });
 
 export default router;
