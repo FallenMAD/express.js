542d2bccf1226fda9fd614373f049a5d182a4b54
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-17 14:33:40 +0200

    feat(app.js): add global configuration to use template engines

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ import shopRoutes from './routes/shop.js';
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
