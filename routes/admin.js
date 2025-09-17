13e547fc732ae65037c011e655aa0453a0dc950b
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-17 14:35:04 +0200

    feat(admin.js): add products arrays and push input value to it

--- routes/admin.js
+++ routes/admin.js
@@ -5,11 +5,15 @@ import { rootDir } from '../utils/dirnameHelper.js';
 
 const router = express.Router();
 
+export const products = [];
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 })
 
 router.post('/add-product', (req, res, next) => {
+  const { title } = req.body;
+  products.push({title});
   res.redirect('/');
 })
 
