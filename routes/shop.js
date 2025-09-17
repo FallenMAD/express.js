658149a9a12f53d7aa9e3efb685eb5dbb866c0b1
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-17 14:38:18 +0200

    chore(shop.js): delete unnecessary imports

--- routes/shop.js
+++ routes/shop.js
@@ -1,7 +1,5 @@
-import path from 'path';
 import express from 'express';
 
-import { rootDir } from '../utils/dirnameHelper.js';
 import { products as adminData } from './admin.js';
 
 const router = express.Router();
