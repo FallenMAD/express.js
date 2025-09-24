a9054bca3752e4c51650df6c9be3d6401a38772c
FallenMAD <naruto.yzymaki4@gmail.com>
2025-09-24 15:40:08 +0200

    refactor(shop.js): add new props to the render method

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,13 @@ import { products as adminData } from './admin.js';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop', { products: adminData, docTitle: 'Shop', path: '/' });
+  res.render('shop', { 
+    products: adminData, 
+    docTitle: 'Shop', 
+    path: '/', 
+    isActiveShop: true, 
+    productCss: true 
+  });
 });
 
 export default router;
